de9beeb6caea66f5eb9409d65b87f4b12fe97c00
Dennys Reyes <dennysgraph@gmail.com>
2023-08-03 17:49:50 -0400

    data fetch

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
+        adult: movieDB.adult,
+        backdropPath: (movieDB.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+            : 'https://privateflite.com/assets/global/img/image-not-found-dark.png',
+        genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+        id: movieDB.id,
+        originalLanguage: movieDB.originalLanguage,
+        originalTitle: movieDB.originalTitle,
+        overview: movieDB.overview,
+        popularity: movieDB.popularity,
+        posterPath: (movieDB.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+            : 'no',
+        releaseDate: movieDB.releaseDate,
+        title: movieDB.title,
+        video: movieDB.video,
+        voteAverage: movieDB.voteAverage,
+        voteCount: movieDB.voteCount,
+      );
+}
